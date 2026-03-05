c05d3572d68a61822de515e9cd7e1f40bb58a9c6
tiger071123 <matsushitatiger07@gmail.com>
2026-03-06 06:34:22 +0900

    version test run 0.2

--- backend/state.py
+++ backend/state.py
@@ -3,6 +3,6 @@ from pathlib import Path
 
 ROOT_DIR = Path(__file__).parent.resolve()
 
-#data_path = ROOT_DIR/"USvideos.csv"
-data_path = ROOT_DIR/"someUSvideos.csv"
+data_path = ROOT_DIR/"USvideos.csv"
+#data_path = ROOT_DIR/"someUSvideos.csv"
 data = pd.DataFrame(pd.read_csv(data_path))
